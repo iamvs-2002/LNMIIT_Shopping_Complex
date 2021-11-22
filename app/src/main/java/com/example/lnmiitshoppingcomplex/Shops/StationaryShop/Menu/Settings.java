c220e22a57e29ba9cfabda079a5ab7d186385cbe
Vaibhav Singhal <iamvs2002@gmail.com>
2021-11-22 15:34:26 +0530

    Updated Menu

---
+++ app/src/main/java/com/example/lnmiitshoppingcomplex/Shops/StationaryShop/Menu/Settings.java
@@ -0,0 +1,16 @@
+package com.example.lnmiitshoppingcomplex.Shops.StationaryShop.Menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lnmiitshoppingcomplex.R;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
